8f4b9b9ea83f44964656bf301dd7fbb8ff00728b
Jawad Hussain <jawadduet@gmail.com>
2024-07-21 10:36:55 +0500

    For of loop

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+//for of works with each iterable
+let fullName = "Jawad Hussain";
 
+
+for (const char of fullName) {
+    console.log(char);
+}
+
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for (const income of incomes) {
+    console.log(income);
+    total += income;
+}
 
 
 
