eab754407392d19447d96b8d2e9ae77050a2f647
Jawad Hussain <jawadduet@gmail.com>
2024-07-21 11:49:54 +0500

    Default params challenge

--- arrowFunction.js
+++ arrowFunction.js
@@ -6,6 +6,25 @@ const leadSinger = (artist = "someone") => {
 
 leadSinger("Khan");
 
+/*
+**** Challenge *****
+
+Create a function that receives a parameter of food.
+If your parameter food is going to have a value of "milk"
+the function should print into the console the following:
+
+"I'm going to buy milk from the grocery shop"
+
+But if you dont pass a value to your parameter food, it should print
+"I'm going to buy something from the grocery shop"
+
+*/
+const buyFood = (food = "something") => {
+    console.log(` I am going to buy ${food} from the grocery shop `);
+}
+
+buyFood("milk");
+buyFood();
 
 
 
